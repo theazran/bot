572623878e05676136e9fde54140043d912e7ba7
M ASRAN <azranmu@gmail.com>
2022-11-01 22:32:50 +0800

    Update server.js
    Co-Authored-By: AMIRAH HISANA ANJU ALFADHILA SALSABILA <109892251+premibulan3@users.noreply.github.com>

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Halo ges!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
